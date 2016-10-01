caeafdba3c924042920ed436a559d57b77fecb14
suraj <sg605597@gmail.com>
2016-10-01 10:00:40 +0530

    commit

---
+++ don.py
@@ -0,0 +1,8 @@
+print("program to identify if the number is even or odd")
+
+numbere = int(input("Enter a number"))
+
+if numbere % 2 == 0:
+         print ("The number is even")
+else:
+        print ("The number is odd")
