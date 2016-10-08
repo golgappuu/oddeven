a419de4cd8d8d0c7d8c57f95ce2b3fc11bc5b555
khushboo <khushboo@gmail.com>
2016-10-08 11:49:02 +0530

    oddeven

--- don.py
+++ don.py
@@ -1,8 +1,9 @@
-print("program to identify if the number is even or odd")
-
-numbere = int(input("Enter a number"))
-
-if numbere % 2 == 0:
-         print ("The number is even")
+def isEven(number)
+    if number % 2 == 0:
+         print ("The is an even number")
 else:
-        print ("The number is odd")
+        print ("The is an odd number")
+
+num=int(input("Enter a number"))
+
+isEven(num)
